b44012c4d667f15465f9f1d734f57bbfe14880d9
nwelchr <nwelchr@gmail.com>
2018-06-22 07:54:12 -0700

    Add permutations

---
+++ permutations.js
@@ -0,0 +1,22 @@
+const permutator = (inputArr) => {
+  let result = [];
+
+  const permute = (arr, m = []) => {
+    debugger;
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+      }
+    }
+  }
+
+  permute(inputArr);
+
+  return result;
+}
+
+console.log(permutator([1, 2, 3, 4]));
