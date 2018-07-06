405494280f8cba08faaa49dce7aa20b01533e421
nwelchr <nwelchr@gmail.com>
2018-07-05 22:32:37 -0700

    Add extra implementation to quick_sort

--- quick_sort/lib/quick_sort.rb
+++ quick_sort/lib/quick_sort.rb
@@ -18,6 +18,8 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+
     return array if length <= 1
 
     # returns place to split length and sorts array 
@@ -37,11 +39,13 @@ class QuickSort
   def self.partition(array, start, length, &prc)
     prc ||= Proc.new { |a, b| a <=> b }
 
-    # # choose random pivot, shuffle that position with the start position
+    # choose random pivot, shuffle that position with the start position
+    # doesn't work for spec
     # pivot_idx = start + rand(length)
     # pivot = array[pivot_idx]
     # array[start], array[pivot_idx] = array[pivot_idx], array[start]
 
+    # first way...
     pivot_idx = start
     pivot = array[start]
 
@@ -54,6 +58,29 @@ class QuickSort
     end
 
     array[start], array[pivot_idx] = array[pivot_idx], array[start]
+
+    ## OR...
+    # pivot_idx, pivot = start, array[start]
+    # ((start + 1)...(start + length)).each do |idx|
+    #   val = array[idx]
+    #   if prc.call(pivot, val) < 1
+    #     # if the element is greater than or equal to the pivot, leave
+    #     # where it is.
+    #   else
+    #     # Three-way shuffle: pivot_idx + 1 => idx, pivot_idx =>
+    #     # pivot_idx + 1, idx => pivot_idx.
+    #
+    #     # move self[pivot_idx + 1] to idx, which keeps this bigger item
+    #     # to the right of the pivot.
+    #     array[idx] = array[pivot_idx + 1]
+    #     # move the pivot forward one, to where the larger item used to live.
+    #     array[pivot_idx + 1] = pivot
+    #     # move the smaller item to one to the left of the pivot.
+    #     array[pivot_idx] = val
+    #
+    #     pivot_idx += 1
+    #   end
+    # end
 
     pivot_idx
   end
