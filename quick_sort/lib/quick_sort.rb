dd336469dce7806243e2c5b02451afe6a4dd462a
nwelchr <nwelchr@gmail.com>
2018-07-05 22:02:25 -0700

    Add random pivot to first quicksort

--- quick_sort/lib/quick_sort.rb
+++ quick_sort/lib/quick_sort.rb
@@ -7,6 +7,9 @@ class QuickSort
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
     return array if array.length < 1
+    new_pivot = rand(array.length)
+    array[0], array[pivot] = array[new_pivot], array[0]
+
     pivot = array.first
     left = array.select{ |val| val < pivot }
     right = array.select{ |val| val >= pivot }
@@ -19,11 +22,11 @@ class QuickSort
 
     # returns place to split length and sorts array 
     # because you pass in the array by reference 
-    pivot_idx = QuickSort.partition(array, start, length, &prc)
+    pivot_idx = partition(array, start, length, &prc)
     
     # left_length is place to split minus start
     left_length = pivot_idx - start
-    # right_length is just everything else, but since pivot is in right spot keep that
+    # right_length is just everythinng else, but since pivot is in right spot keep that
     right_length = length - (left_length + 1)
     sort2!(array, start, left_length, &prc) 
     sort2!(array, pivot_idx + 1, right_length, &prc)
